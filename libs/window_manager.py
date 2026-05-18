fd0f843f633d292aea7f402ee2b9051e628f0111
Remmie <chinemeremokolie12@gmail.com>
2026-05-18 16:55:48 +0100

    fix: remove broken imports from window_manager and pass root as parameter

--- libs/window_manager.py
+++ libs/window_manager.py
@@ -1,22 +1,17 @@
-from libs.common import *
-from libs.helpers import *
+import tkinter as tk
 
-frames:list[tk.Frame]=[]
+frames: list[tk.Frame] = []
 
-def create_frame(root:tk.Tk):
-    frame=tk.Frame(root)
-    frames.append(frame)
-    return frame
-
-def show_frame(f:tk.Frame):
-    for i in frames:
-        i.pack_forget()
-    f.pack()
-
+def show_frame(f: tk.Frame):
+    for frame in frames:
+        frame.pack_forget()
+    f.pack(fill="both", expand=True)
 
 class Screen:
-    def __init__(self):
-        self.frame=tk.Frame(root)
+    def __init__(self, root: tk.Tk):
+        self.root = root
+        self.frame = tk.Frame(root)
         frames.append(self.frame)
+
     def show(self):
         show_frame(self.frame)

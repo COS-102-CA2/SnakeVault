e06baf22f2d4911f400cd6958b716071ea1c73b0
Remmie <chinemeremokolie12@gmail.com>
2026-05-20 00:22:13 +0100

    fix: replace legacy password store with crypto helpers

--- libs/crypto.py
+++ libs/crypto.py
@@ -1,39 +1,55 @@
-import shelve
+import base64
 import hashlib
 import os
 
-class PasswordStore:
-    def __init__(self,  master_key):
-        self._db_file = '~/user_passwords'
-        self._master_key = master_key.encode('utf-8')
-
-    def store_password(self, identifier, password):
-        salt = os.urandom(16)
-        pwd_bytes = password.encode('utf-8')
-        key_stream = hashlib.pbkdf2_hmac('sha256', self._master_key, salt, 100000, len(pwd_bytes))
-        encrypted = bytes(a ^ b for a, b in zip(pwd_bytes, key_stream))
-        with shelve.open(self._db_file) as db:
-            db[identifier] = salt + encrypted
-
-    def retrieve_password(self, identifier):
-        with shelve.open(self._db_file) as db:
-            data = db.get(identifier)
-            if not data:
-                return None
-        salt = data[:16]
-        encrypted = data[16:]
-        key_stream = hashlib.pbkdf2_hmac('sha256', self._master_key, salt, 100000, len(encrypted))
-        decrypted = bytes(a ^ b for a, b in zip(encrypted, key_stream))
-        return decrypted.decode('utf-8')
-
-    def remove_password(self, identifier):
-        with shelve.open(self._db_file) as db:
-            if identifier in db:
-                del db[identifier]
-
-    def list_websites(self):
-        with shelve.open(self._db_file) as db:
-            return list(db.keys())
-        
+import bcrypt
 
 
+def hash_key(master_key: str) -> str:
+    key_bytes = master_key.encode("utf-8")
+    hashed = bcrypt.hashpw(key_bytes, bcrypt.gensalt())
+    return hashed.decode("utf-8")
+
+
+def verify_key(master_key: str, stored_hash: str) -> bool:
+    key_bytes = master_key.encode("utf-8")
+    hash_bytes = stored_hash.encode("utf-8")
+    return bcrypt.checkpw(key_bytes, hash_bytes)
+
+
+def encrypt(password: str, master_key: str) -> str:
+    key_bytes = master_key.encode("utf-8")
+    pwd_bytes = password.encode("utf-8")
+
+    salt = os.urandom(16)
+    key_stream = hashlib.pbkdf2_hmac(
+        "sha256",
+        key_bytes,
+        salt,
+        100000,
+        len(pwd_bytes),
+    )
+
+    encrypted = bytes(a ^ b for a, b in zip(pwd_bytes, key_stream))
+    result = base64.b64encode(salt + encrypted).decode("utf-8")
+
+    return result
+
+
+def decrypt(encrypted_b64: str, master_key: str) -> str:
+    key_bytes = master_key.encode("utf-8")
+
+    raw = base64.b64decode(encrypted_b64.encode("utf-8"))
+    salt = raw[:16]
+    encrypted = raw[16:]
+
+    key_stream = hashlib.pbkdf2_hmac(
+        "sha256",
+        key_bytes,
+        salt,
+        100000,
+        len(encrypted),
+    )
+
+    decrypted = bytes(a ^ b for a, b in zip(encrypted, key_stream))
+    return decrypted.decode("utf-8")

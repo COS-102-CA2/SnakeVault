2ba63b922ae136cdfcfc1866dbc9a7393984243d
Remmie <chinemeremokolie12@gmail.com>
2026-05-20 00:02:59 +0100

    refactor: wire app router and shared master key state

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
 import tkinter as tk
+
+from screens.welcome import WelcomeScreen
 from screens.login_screen import LoginScreen
+from screens.create_key import CreateKeyScreen
+from screens.confirm_key import ConfirmKeyScreen
+from screens.setup_done import SetupDoneScreen
+from screens.unlock import UnlockScreen
 from screens.dashboard_screen import DashboardScreen
+from screens.add_password import AddPasswordScreen
+from screens.password_detail import PasswordDetailScreen
+from screens.generator import GeneratorScreen
+from screens.search import SearchScreen
+from screens.settings import SettingsScreen
+
 
 class SnakeVaultApp(tk.Tk):
     def __init__(self):
         super().__init__()
+
         self.title("SnakeVault - Password Manager")
         self.geometry("800x550")
-        self.configure(bg="#1e1e2e")  # Modern dark theme background
-        
-        # Container frame to hold different screens
-        self.container = tk.Frame(self, bg="#1e1e2e")
+        self.configure(bg="#1E1E2E")
+
+        self.container = tk.Frame(self, bg="#1E1E2E")
         self.container.pack(fill="both", expand=True)
 
-        # Track the active screen
         self.current_screen = None
-        
-        # Show login screen on startup
-        self.show_screen("login")
+        self.master_key = None
 
-    def show_screen(self, screen_name):
-        """Destroys the current screen and loads the requested one."""
+        self.show_screen("welcome")
+
+    def show_screen(self, screen_name, **kwargs):
         if self.current_screen is not None:
             self.current_screen.destroy()
 
-        if screen_name == "login":
-            self.current_screen = LoginScreen(self.container, self)
-        elif screen_name == "dashboard":
-            self.current_screen = DashboardScreen(self.container, self)
-            
-        self.current_screen.pack(fill="both", expand=True)
+        screens = {
+            "welcome": lambda: WelcomeScreen(self.container, self),
+            "login": lambda: LoginScreen(self.container, self),
+            "create_key": lambda: CreateKeyScreen(self.container, self),
+            "confirm_key": lambda: ConfirmKeyScreen(self.container, self, **kwargs),
+            "setup_done": lambda: SetupDoneScreen(self.container, self),
+            "unlock": lambda: UnlockScreen(self.container, self),
+            "dashboard": lambda: DashboardScreen(self.container, self),
+            "add_password": lambda: AddPasswordScreen(self.container, self),
+            "password_detail": lambda: PasswordDetailScreen(self.container, self, **kwargs),
+            "generator": lambda: GeneratorScreen(self.container, self),
+            "search": lambda: SearchScreen(self.container, self),
+            "settings": lambda: SettingsScreen(self.container, self),
+        }
+
+        if screen_name in screens:
+            self.current_screen = screens[screen_name]()
+            self.current_screen.pack(fill="both", expand=True)
+
 
 if __name__ == "__main__":
     app = SnakeVaultApp()
